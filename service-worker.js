3d7df0ae06722c805b90c416188f72f245998ecb
yennerrodriguez-jpg <mynameisyenner@proton.me>
2026-01-01 15:26:06 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('omicron-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
